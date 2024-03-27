c4cd996b0675a247dbd5f5a3287fd9dee31ddc59
Jayza187 <brunopenner01@gmail.com>
2024-03-27 15:02:19 +0800

    commit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,20 @@
 import { Link } from 'react-router-dom';
 import LogoTitle from '../..//assets/images/logo-s.png';
 import './index.scss';
+import { useState } from 'react';
+import { AnimatedLetters } from '../AnimatedLetters';
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['o', 'a', 'r', 'e', 's']
+    const jobArray = ['w','e','b','','d','e','v','e','l','o','p','e','r','.']
+
     return (
         <div className="container home-page">
             <div className="text-zone">
                 <h1>Hi, <br /> I'm
                 <img src={LogoTitle} alt="developer"/>
-                oares
+                <AnimatedLetters  />
                 <br />
                 Web developer
                 </h1>
