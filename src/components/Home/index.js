eb70e41e46e36ce5b6dc5f80629b854b38a33cca
Jayza187 <brunopenner01@gmail.com>
2024-03-28 10:04:46 +0800

    Fixed import AnimateLetters - before it was import { AnimatedLetters }, but the latter don't work since AnimatedLetters is an export default

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,22 +1,29 @@
 import { Link } from 'react-router-dom';
-import LogoTitle from '../..//assets/images/logo-s.png';
+import LogoTitle from '../../assets/images/logo-s.png';
 import './index.scss';
 import { useState } from 'react';
-import { AnimatedLetters } from '../AnimatedLetters';
+import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
     const nameArray = ['o', 'a', 'r', 'e', 's']
-    const jobArray = ['w','e','b','','d','e','v','e','l','o','p','e','r','.']
+    const jobArray = ['w','e','b',' ','d','e','v','e','l','o','p','e','r','.']
 
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm
+                <h1>
+                <span className={letterClass}>H</span>
+                <span className={letterClass}>H</span>
+                <br /> I'm
                 <img src={LogoTitle} alt="developer"/>
-                <AnimatedLetters  />
+                <AnimatedLetters letterClass={letterClass} 
+                strArray={nameArray}
+                idx={15} />
                 <br />
-                Web developer
+                <AnimatedLetters letterClass={letterClass} 
+                strArray={jobArray}
+                idx={22} />
                 </h1>
                 <h2>Fronted Developer / JavaScript Expert / YouTuber</h2>
                 <Link to="contar" className='flat-button'>CONTACT ME</Link>
